33d5e13ab38b9eb5dc0bc435ab3140ec935a01de
Keith <keithwhisper@gmail.com>
2018-08-05 16:24:46 +0800

    update: update contract

--- first_forever/src/contracts/contracts.js
+++ first_forever/src/contracts/contracts.js
@@ -2,22 +2,33 @@ const nervos = require('../nervos')
 const {
   abi
 } = require('./compiled')
+const {
+  contractAddress
+} = require('../config')
 const transaction = require('./transaction')
 
-const simpleStoreContract = new nervos.appchain.Contract(abi, '0x58C688Bbb76187aa92c13DF0395a578b45aDA7d9')
+const simpleStoreContract = new nervos.appchain.Contract(abi, contractAddress)
 
 
 nervos.appchain.getBalance(nervos.eth.accounts.wallet[0].address).then(console.log)
+console.log(`Interact with contract at ${contractAddress}`)
+const text = 'hello world'
+const time = new Date().getTime()
 nervos.appchain.getBlockNumber().then(current => {
   transaction.validUntilBlock = +current + 88
-  return simpleStoreContract.methods.add('123', 1).send(transaction)
+  console.log(`Add record of (${text}, ${time})`)
+  return simpleStoreContract.methods.add(text, time).send(transaction)
 }).then(receipt => {
   console.log(receipt)
   return nervos.listeners.listenToTransactionReceipt(receipt.hash)
 }).then(res => {
   console.log(res)
   setTimeout(() => {
-    simpleStoreContract.methods.getList().call().then(console.log)
-    simpleStoreContract.methods.get(1).call().then(console.log)
+    simpleStoreContract.methods.getList().call({
+      from: transaction.from
+    }).then(console.log)
+    simpleStoreContract.methods.get(time).call({
+      from: transaction.from
+    }).then(console.log)
   }, 6000)
 })
