6f53c3dc75361051f1336efb0c5410aa052f17d4
Keith <keithwhisper@gmail.com>
2018-08-06 10:51:18 +0800

    fix: fix time list display

--- first_forever/src/containers/List/index.jsx
+++ first_forever/src/containers/List/index.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import BottomNav from '../../components/BottomNav'
 import { simpleStoreContract } from '../../simpleStore'
 import nervos from '../../nervos'
-import transaction from '../../contracts/transaction'
 require('./list.css')
 
-const Record = ({ time, text }) => {
+const Record = ({ time, text, hasYearLabel }) => {
   const _time = new Date(+time)
   return (
     <div className="list__record--container">
+      {hasYearLabel ? <div className="list__record--year">{_time.getFullYear()}</div> : null}
       <span>{`${_time.getMonth() + 1}-${_time.getDate()} ${_time.getHours()}:${_time.getMinutes()}`}</span>
       <Link to={`/show/${time}`}>
         <div>{text}</div>
@@ -44,7 +44,14 @@ class List extends React.Component {
     const { times, texts } = this.state
     return (
       <div className="list__record--page">
-        {times.map((time, idx) => <Record time={time} text={texts[idx]} key={time} />)}
+        {times.map((time, idx) => (
+          <Record
+            time={time}
+            text={texts[idx]}
+            key={time}
+            hasYearLabel={idx === 0 || new Date(+time).getFullYear() !== new Date(+times[idx - 1]).getFullYear()}
+          />
+        ))}
         <BottomNav active={'list'} />
       </div>
     )
