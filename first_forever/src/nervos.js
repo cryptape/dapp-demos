44e925fc9450cf04e94aeb7e15c489f05df641e3
duanyytop <duanyytop@gmail.com>
2018-08-15 22:25:41 +0800

    add timeout

--- first_forever/src/nervos.js
+++ first_forever/src/nervos.js
@@ -3,16 +3,17 @@ const {
 } = require('@nervos/web3')
 
 const config = require('./config')
-
-// const nervos = Nervos(config.chain) // config.chain indicates that the address of Appchain to interact
-if (typeof window.nervos !== 'undefined') {
-  window.nervos = Nervos(window.nervos.currentProvider);
-  window.nervos.currentProvider.setHost("https://node.cryptape.com");
-} else {
-  console.log('No Nervos web3? You should consider trying Neuron!')
-  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  window.nervos = Nervos(config.chain);
-}
-var nervos = window.nervos
+var nervos
+setTimeout(function() {
+  if (typeof window.nervos !== 'undefined') {
+    window.nervos = Nervos(window.nervos.currentProvider);
+    window.nervos.currentProvider.setHost("https://node.cryptape.com");
+  } else {
+    console.log('No Nervos web3? You should consider trying Neuron!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.nervos = Nervos(config.chain);
+  }
+  nervos = window.nervos
+}, 2000);
 
 module.exports = nervos
