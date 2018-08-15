c9c5091782e0b33c14fbd6ccc1bcd411b8b5b6b9
duanyytop <duanyytop@gmail.com>
2018-08-15 23:14:43 +0800

    delete time out

--- first_forever/src/nervos.js
+++ first_forever/src/nervos.js
@@ -3,17 +3,14 @@ const {
 } = require('@nervos/web3')
 
 const config = require('./config')
-var nervos
-setTimeout(function() {
-  if (typeof window.nervos !== 'undefined') {
-    window.nervos = Nervos(window.nervos.currentProvider);
-    window.nervos.currentProvider.setHost("https://node.cryptape.com");
-  } else {
-    console.log('No Nervos web3? You should consider trying Neuron!')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.nervos = Nervos(config.chain);
-  }
-  nervos = window.nervos
-}, 2000);
+if (typeof window.nervos !== 'undefined') {
+  window.nervos = Nervos(window.nervos.currentProvider);
+  window.nervos.currentProvider.setHost("https://node.cryptape.com");
+} else {
+  console.log('No Nervos web3? You should consider trying Neuron!')
+  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+  window.nervos = Nervos(config.chain);
+}
+var nervos = window.nervos
 
 module.exports = nervos
