f1e2792319cb13f7bac51da9ba8daf3a1a0ea63d
baojun <bj805887485@aliyun.com>
2018-08-23 10:41:26 +0800

    add README

--- token-factory/src/contracts/tokenStore.js
+++ token-factory/src/contracts/tokenStore.js
@@ -60,18 +60,18 @@ export const getTokenContract = function(contractAddress) {
 
 export const getTX = () =>
   nervos.appchain.getBlockNumber().then(current => {
-    const tx = {
-      ...transaction,
-      from: "0x23a659e53b55ca1ba5ce741e12420cb64147f5d7",
-      validUntilBlock: +current + 88,
-      privateKey:
-        "1208A2E6F122F64C2C4C380781A19D68DDD3CE614E3A688526548A0D61ABF0B1"
-    };
     // const tx = {
     //   ...transaction,
-    //   from: window.neuron.getAccount(),
-    //   validUntilBlock: +current + 88
+    //   from: "your address",
+    //   validUntilBlock: +current + 88,
+    //   privateKey:
+    //     "your private key"
     // };
+    const tx = {
+      ...transaction,
+      from: window.neuron.getAccount(),
+      validUntilBlock: +current + 88
+    };
     return tx;
   });
 
